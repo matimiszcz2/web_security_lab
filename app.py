bf8a0bae91b1ecf7161f514db5154c6fdc77ba6e
Maciek Droździel <66441580+MaDo-DoMa@users.noreply.github.com>
2025-06-17 13:46:08 +0200

    dodanie session_hijack_demo i aktywacja w app

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, request
 from modules.sql import sql_injection_view
 from modules.LFI import lfi_view
-from modules.session_demo import login_view, profile_view
+from modules.session_demo import login_view as demo_login, profile_view as demo_profile
+from modules.session_hijack_demo import(
+    login_unsafe,
+    login_safe,
+    profile_view as hijack_profile,
+    logout_view
+)
 
 app = Flask(__name__)
 
 #ustawienia klucza dla sesji
 app.secret_key = 'MgrZaworskiBylNaErazmusie2137'
 
-app.add_url_rule('/login', view_func=login_view, methods=['GET', 'POST'])
-app.add_url_rule('/profile', view_func=profile_view, methods=['GET', 'POST'])
+app.add_url_rule('/login', view_func=demo_login(), methods=['GET', 'POST'])
+app.add_url_rule('/profile', view_func=demo_profile(), methods=['GET', 'POST'])
+app.add_url_rule('/logout', view_func=logout_view())
+app.add_url_rule('/login-unsafe', view_func=login_unsafe(), methods=['GET', 'POST'])
+app.add_url_rule('login-safe', view_func=login_safe(), methods=['GET', 'POST'])
+app.add_url_rule('/profile-hijack', view_func= hijack_profile())
 
 # Główna strona
 @app.route('/')
