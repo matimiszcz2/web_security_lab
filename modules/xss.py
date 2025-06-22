5131d6d94cb22d1959e1aa10631703b4bc3a8c0b
Maciek Droździel <66441580+MaDo-DoMa@users.noreply.github.com>
2025-06-22 13:15:25 +0200

    poprawa plików

---
+++ modules/xss.py
@@ -0,0 +1,18 @@
+from flask import request, session, redirect, url_for, render_template
+
+def login_view():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if username == 'admin':
+            session.permanent = True
+            session['user'] = username
+            return redirect(url_for('profile_view'))
+
+    # ⬇️ Tu renderujesz stronę logowania
+    return render_template('xss.html')
+
+def profile_view():
+    user = session.get('user')
+    if user:
+        return f"Siema, {user}!"
+    return "Nie jesteś zalogowany"
